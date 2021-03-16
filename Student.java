3442f9bce389cc7fab2db28ca574a0cdcf8818e8
JSylleLoreineTubola <tubola.jsylle@gmail.com>
2021-03-16 18:20:25 +0800

    Constructor

--- Student.java
+++ Student.java
@@ -1,13 +1,18 @@
 import java.util.*;
 public class Student {
-    public int ID;
-    public int hrs;
-    public int pts;
+    private int ID;
+    private int pts;
+    private int hrs;
+    private double gp;
 
-    public Student(int ID, int hrs, int pts) {
+    public Student() {
+    }
+
+    public Student(int ID, int pts, int hrs) {
         this.ID = ID;
-        this.hrs = hrs;
         this.pts = pts;
+        this.hrs = hrs;
+        calculateGradePoint();
     }
 
     public int getID() {
@@ -33,4 +38,14 @@ public class Student {
     public void setPts(int pts) {
         this.pts = pts;
     }
+
+   @Override
+   public String toString() {
+       return "\n ID number : " + ID + "\n Number of points earned : " + pts + "\n Credit hours : " + hrs + "\n Grade point average : "
+               + gp;
+   }
+
+   public void calculateGradePoint() {
+       gp = pts / (double) hrs;
+   }
 }
